25937e4bc3903285697550b5a3cd9573a42fd8ca
tom.schreiber@tomhost.de <tom.schreiber@tomhost.de@265f038a-b1eb-9fa7-ff71-d764355a289f>
2011-06-15 19:33:58 +0000

    add polish translation (now really)
    git-svn-id: http://google-mail-oex.googlecode.com/svn/trunk@85 265f038a-b1eb-9fa7-ff71-d764355a289f

---
+++ locales/pl/lang.js
@@ -0,0 +1,43 @@
+// init language object (do)
+var lang = new Object();
+
+// set default strings
+lang.options_getverifiy  = "pobierz kod";
+lang.options_saveverifiy = "użyj kodu";
+lang.options_delete = "usuń";
+lang.options_update = "Sprawdzaj co";
+lang.options_check = "jest sprawdzane";
+lang.options_wait = "Proszę czekać...";
+lang.options_vcode = "Kod weryfikacyjny";
+lang.options_failverify = "Kod weryfikacyjny jest niepoprawny.";
+lang.options_unread = "wszystkie nowe";
+lang.options_more_vcode = "Dodaj kolejne kody weryfikacyjne (wiele kont)";
+lang.options_sound = "Odtwarzaj dźwięk przy nowych wiadomościach";
+lang.options_mailto = "Otwieraj odnośniki mailowe (mailto) w Gmailu";
+lang.options_close = "Zapisz i zamknij okno";
+lang.options_choose_theme   = "Choose Theme";
+			       
+lang.options_unread_tooltip = "zaznacz to pole aby pokazywać wszystkie nowe wiadomości, nie tylko ze skrzynki odbiorczej";
+lang.options_vcode_tooltip = "wklej lub przepisz kod weryfikacyjny, a następnie kliknij 'użyj kodu'";
+lang.options_delete_tooltip = "kliknij tu aby przestać sprawdzać ten adres e-mail";
+
+lang.popup_open = "Otwórz <strong>GMail</strong> w nowej karcie";
+lang.popup_compose = "Utwórz wiadomość";
+lang.popup_pref = "Ustawienia";
+
+lang.popup_nomsg = "<strong>Brak nowych wiadomości</strong>";
+lang.popup_onemsg = "Jest <strong>jedna nowa wiadomość</strong>";
+lang.popup_msg_before = "Są <strong>nowe (";
+lang.popup_msg_after = ") wiadomości</strong>";
+lang.popup_lastupdate = "Ostatnia aktualizacja : ";
+lang.popup_error_occurred="wystąpił błąd";
+lang.popup_errors_occurred="wystąpiły błędy";
+lang.popup_from = "Od: ";
+lang.popup_to = "Do: ";
+
+lang.error_confails = "<strong>Wystąpił błąd. </strong> " +
+  "Sprawdź swoje połączenie oraz <a href='javascript:ShowPreferences();'>ustawienia</a>";
+lang.error_nocode = "<strong>Brak skonfigurowanego konta e-mail</strong>, " +
+  "<br/>zmień swoje <a href='javascript:ShowPreferences();'>ustawienia</a>";
+lang.error_getfeed = "Błąd przy pobieraniu wiadomości";
+
