02fb2e56ccabbfb947a3ef99c4bea2c7c3fd119f
tom.schreiber@tomhost.de <tom.schreiber@tomhost.de@265f038a-b1eb-9fa7-ff71-d764355a289f>
2012-02-10 20:31:44 +0000

    mailto-option supports multiple accounts
    git-svn-id: http://google-mail-oex.googlecode.com/svn/trunk@134 265f038a-b1eb-9fa7-ff71-d764355a289f

--- includes/mailto.js
+++ includes/mailto.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	Google Mail Notfier - Mailto-Links
 // @description	Open mailto-links with gmail
-// @include	*
+// @include	http*
 // @copyright	codebit
 // @version	1.0.0
 // ==/UserScript==
@@ -9,35 +9,129 @@
 (function () 
 {
     var EnableLink = false;
+    var Accounts;
+    var AccountsCount = 0;
 
-    // set listener for option change
-    opera.extension.addEventListener("message", function(e) {
-        if(e.data.cmd == "MailtoEnabled")
-        {
-            if(e.data.value == "on")
-                EnableLink = true;
-            else
-                EnableLink = false;
-        }
-    }, false);
+    // Set listener for Messages from background-process
+    opera.extension.onmessage = HandleMessages;    
 
-    // get current option
-    opera.extension.postMessage({cmd: "MailtoEnabled"});
+    // Get current Option
+    opera.extension.postMessage({
+        cmd: "MailtoEnabled"
+    });
+    
+    // Get current Accounts
+    opera.extension.postMessage({
+        cmd:"Refresh_NoCallback"
+    });
 
     // Look at mailto-links
-    window.addEventListener("load",function(){
-        if(EnableLink)
+    window.addEventListener("load", ManipulateLinks, false);
+    
+    // Handle messages from background-process
+    function HandleMessages(event)
+    {
+
+        switch(event.data.cmd)
+        {            
+            // Get Option
+            case "MailtoEnabled":
+                if(event.data.value == "on")
+                    EnableLink = true;
+                else
+                    EnableLink = false;    
+                break;
+            
+            // Get Info-Message, thats means there are no active connection :(
+            case "info" :
+                AccountsCount = 0;
+                ManipulateLinks();
+                break;
+    
+            // Messages
+            case "messages":
+                Accounts = event.data.accounts;
+                AccountsCount = event.data.accounts_count;
+                if(AccountsCount > 0) ManipulateLinks();
+                break;            
+        }
+    }
+    
+    // Manipulate Links
+    function ManipulateLinks()
+    {
+        if(EnableLink && document.body)
         {
             var links = document.body.getElementsByTagName("a");
             for(var i in links)
             {
                 if(links[i].href && links[i].href.substr(0,7) == "mailto:")
                 {
-                    links[i].href = links[i].href.replace("mailto:","https://mail.google.com/mail/?extsrc=mailto&url=mailto:");
-                    links[i].setAttribute("target", "_blank");
+                    // One Account 
+                    if(AccountsCount == 1)
+                    {                         
+                        links[i].href = links[i].href.replace("mailto:", "https://mail.google.com/mail/?extsrc=mailto&url=mailto:");
+                        links[i].setAttribute("target", "_blank");
+                    }
+                    // Multiple Accounts
+                    else
+                        ChooseAccountBox(links[i], "gmn_mailto" + i);
                 }
             }
+        }        
+    }
+    
+    // Dialogbox for choosing Account
+    function ChooseAccountBox(element, id)
+    {
+        if(AccountsCount > 0)
+        {
+            // Create Box for choosing Account
+            var box = document.createElement('div');
+            box.setAttribute('id', id);
+            box.style.position = "absolute"; 
+            box.style.display = "none";
+            box.style.border = "1px solid #DDD";
+            box.style.borderRadius = "5px";
+            box.style.padding = "5px";
+            box.style.background = "rgba(255,255,255,0.7)";            
+            box.style.fontFamily = element.currentStyle['fontFamily'];
+            box.style.fontSize = "0.8em";
+            
+            var header = document.createElement('p');
+            header.innerHTML = "Google Mail Account";
+            header.fontSize = "0.8em";
+            header.style.fontWeight = "bold";
+            box.appendChild(header);            
+            
+            // Create entrys
+            for(var mail in Accounts)
+            {
+                if(Accounts[mail])
+                {
+                    var entry = document.createElement('a');
+                    entry.style.display = "block"
+                    entry.style.marginLeft = "8px";
+                    entry.innerHTML = Accounts[mail].Name;
+                    entry.href = element.href.replace("mailto:", Accounts[mail].AccountLink + "/?extsrc=mailto&url=mailto:");
+                    box.appendChild(entry);
+                }
+            }              
+                        
+            // Append box
+            document.body.appendChild(box);
+            
+            // Set function to show/hide box
+            element.onclick = function(e) {
+                var box = document.getElementById(id);         
+                box.style.left = (e.pageX - 20) + "px";
+                box.style.top = (e.pageY - 10) + "px";
+                box.style.display = "block";
+                box.onmouseleave = function(){
+                    this.style.display = "none";
+                }
+                return false;
+            }  
         }
-    },false);
-
+    }
 })();
