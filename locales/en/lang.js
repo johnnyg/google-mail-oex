678c62effc548b8003efc965515dddc6246f080e
tom.schreiber@tomhost.de <tom.schreiber@tomhost.de@265f038a-b1eb-9fa7-ff71-d764355a289f>
2012-10-10 09:20:19 +0000

    updated en/lang.js
    git-svn-id: http://google-mail-oex.googlecode.com/svn/trunk@231 265f038a-b1eb-9fa7-ff71-d764355a289f

--- locales/en/lang.js
+++ locales/en/lang.js
@@ -1,20 +1,21 @@
-/* Google Mail Notifier - English Translation (Default)
+/* GMail Notifier - English Translation (Default)
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * by Tom Schreiber (tom@codebit.de)
 
 SHORT DESCRIPTION FOR ADDONS.OPERA.COM (max 220):
-A simple and fast extensions to inform you about unread messages in your GMail-Account and show the titles of the new messages. You can check multiple accounts and have the option to open mail-links directly with GMail.
+A fast extensions to inform you about unread messages in your GMail-Account(s) with option to achive, delete, etc. the message directly. Your Accounts will be detected automatically when you are logged in.
 
 DETAILED DESCRIPTION FOR ADDONS.OPERA.COM:
-Google Mail Notifier is a small, simple and fast extension to show the number of unread mails in your Gmail-Account(s) in front of a button and the titles of the messages in the button-menu. Furthermore this extension gives you the option to open mail-links (mailto:) directly with GMail.
+GMail Notifier is a small, simple and fast extension to show the number of unread mails in your Gmail-Account(s) in front of a button and the titles of the messages in the button-menu. Furthermore this extension gives you the option to open mail-links (mailto:) directly with GMail.
 
 Features:
-* Automatically detects open GMail-Accounts (no password, username required)
+* Automatically detects open GMail-Accounts (no password or username required)
 * Support Multi-Account
-* Supports all Google Accounts
 * Show all unread Messages or just from Inbox
+* Option to archive, delete, mark as read
 * Option to open Mail-Links directly with GMail
-* Optional Sound-Notification
+* Option to hide Ads on GMail-Page
+* Sound-Notifications, Themes and many more
  
  */
 
